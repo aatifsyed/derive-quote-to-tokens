293bc7f6861573ab89f84a4898d895ff7a3d19e9
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-20 10:03:12 +0100

    feat: initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use syn_helpers::{
+    derive_trait,
+    proc_macro2::Span,
+    syn::{parse_macro_input, parse_quote, DeriveInput, Ident, Stmt},
+    Constructable as _, FieldMut as _, Trait, TraitItem, TypeOfSelf,
+};
+
+#[proc_macro_derive(ToTokens, attributes(to_tokens))]
+pub fn derive_to_tokens(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let item = parse_macro_input!(input as DeriveInput);
+    derive_trait(item, to_tokens()).into()
+}
+
+fn to_tokens() -> Trait {
+    Trait {
+        name: parse_quote!(::quote::ToTokens),
+        generic_parameters: None,
+        items: vec![TraitItem::Method {
+            name: Ident::new("to_tokens", Span::call_site()),
+            generic_parameters: None,
+            self_type: TypeOfSelf::Reference,
+            other_parameters: vec![parse_quote!(tokens: &mut ::proc_macro2::TokenStream)],
+            return_type: None,
+            handler: Box::new(|mut item| {
+                item.map_constructable(|mut constructable| {
+                    Ok(constructable
+                        .get_fields_mut()
+                        .fields_iterator_mut()
+                        .flat_map(|mut field| -> Option<Stmt> {
+                            let field = field.get_reference();
+                            Some(parse_quote!(::quote::ToTokens::to_tokens(#field, tokens);))
+                        })
+                        .collect())
+                })
+            }),
+        }],
+    }
+}
+
+#[cfg(test)]
+fn do_test(input: DeriveInput, expected: syn_helpers::proc_macro2::TokenStream) {
+    use pretty_assertions::assert_eq;
+    use prettyplease::unparse;
+    use syn_helpers::syn::{parse2, File};
+
+    let actual = parse2::<File>(derive_trait(input, to_tokens())).expect("invalid output");
+    let expected = parse2::<File>(expected).expect("invalid input");
+
+    assert_eq!(unparse(&actual), unparse(&expected))
+}
+
+#[test]
+fn test() {
+    use syn_helpers::quote;
+    do_test(
+        parse_quote!(
+            struct Foo {
+                a: Ident,
+                b: String,
+            }
+        ),
+        quote! {
+            #[automatically_derived]
+            impl ::quote::ToTokens for Foo {
+                fn to_tokens(&self, tokens: &mut ::proc_macro2::TokenStream) {
+                    let Foo { a: ref _0, b: ref _1 } = self;
+                    ::quote::ToTokens::to_tokens(_0, tokens);
+                    ::quote::ToTokens::to_tokens(_1, tokens);
+                }
+            }
+        },
+    );
+
+    do_test(
+        parse_quote!(
+            enum Either {
+                Left(Ident),
+                Right { named: Ident },
+            }
+        ),
+        quote! {
+            #[automatically_derived]
+            impl ::quote::ToTokens for Either {
+                fn to_tokens(&self, tokens: &mut ::proc_macro2::TokenStream) {
+                    match self {
+                        Either::Left(ref _0) => {
+                            ::quote::ToTokens::to_tokens(_0, tokens);
+                        }
+                        Either::Right { named: ref _0 } => {
+                            ::quote::ToTokens::to_tokens(_0, tokens);
+                        }
+                    }
+                }
+            }
+        },
+    );
+}
